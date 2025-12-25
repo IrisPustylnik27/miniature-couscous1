4fc1a3e2773ffc42f9027c27960143255fb9ce33
ksenia <kseniay@mail.tau.ac.il>
2025-12-25 14:30:55 +0400

    little changes here and there, made cosmetic changes too

--- AVLTree.py
+++ AVLTree.py
@@ -14,6 +14,7 @@ class AVLNode(object):
     @type value: string
     @param value: data of your node
     """
+    #O(1)
     def __init__(self, key, value):
         self.key = key
         self.value = value
@@ -28,9 +29,9 @@ class AVLNode(object):
     @rtype: bool
     @returns: False if self is a virtual node, True otherwise.
     """
+    #O(1)
     def is_real_node(self):
-        if self.left.key is None and self.right.key is None: return False
-        return True
+        return self.key is not None
 
 
 """
@@ -42,14 +43,13 @@ class AVLTree(object):
     """
     Constructor, you are allowed to add more fields.
     """
-    # add a size for the whole tree, need to update ++ in insert for every insert
-    # add external leaf for simple use
+    #O(1)
     def __init__(self):
         self.sizeOfTree = 0
         self.root = None
         self.exLeaf = AVLNode(None, None)
 
-    #creating new node with height 0 for not repeating the code
+    #O(1)
     def new_node(self, key, value):
         newNode = AVLNode(key, value)
         newNode.height = 0
@@ -65,22 +65,9 @@ class AVLTree(object):
     and e is the number of edges on the path between the starting node and ending node+1.
     """
 
-    #does it need to be 1 when we searched for key in root?
+    #calls O(logn)
     def search(self, key):
         return self.down_search(self.root, key)
-
-    def down_search(self, currNode, key):
-        if currNode is None: return None, -1
-        e = 1
-        while currNode is not None:
-            if currNode.key == key:
-                return currNode, e
-            elif currNode.key < key:
-                currNode = currNode.right
-            else:
-                currNode = currNode.left
-            e += 1
-        return None, -1
 
 
     """searches for a node in the dictionary corresponding to the key, starting at the max
@@ -91,17 +78,44 @@ class AVLTree(object):
     @returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
     and e is the number of edges on the path between the starting node and ending node+1.
     """
+    #O(logn)
     def finger_search(self, key):
         e1 = 1
         currNode = self.max_node()
-        if currNode is None: return None, -1
-        if currNode.key < key: return None, -1
+        if currNode is None: return None, 1
+        if currNode.key < key: return None, 1
         while currNode.key > key:
             if currNode.parent is None: break
             currNode = currNode.parent
             e1 += 1
         node, e2 = self.down_search(currNode, key)
         return node, e1+e2-1
+
+    """searches for a node in the dictionary corresponding to the key, starting at the given node
+
+        @type key: int
+        @param key: a key to be searched
+        @type currNode: AVLNode
+        @param currNode: starting node for searching
+        @rtype: (AVLNode,int)
+        @returns: a tuple (x,e) where x is the node corresponding to key (or None if not found),
+        and e is the number of edges on the path between the starting node and ending node+1.
+        """
+
+    #O(logn)
+    def down_search(self, currNode, key):
+        if currNode is None: return None, 1
+        e = 1
+        while currNode is not None:
+            if currNode.key == key:
+                return currNode, e
+            elif currNode.key < key:
+                currNode = currNode.right
+            else:
+                currNode = currNode.left
+            e += 1
+        return None, e
+
 
 
     """inserts a new node into the dictionary with corresponding key and value (starting at the root)
@@ -116,6 +130,7 @@ class AVLTree(object):
     e is the number of edges on the path between the starting node and new node before rebalancing,
     and h is the number of PROMOTE cases during the AVL rebalancing
     """
+    #calls O(logn)
     def insert(self, key, val):
         x = self.root
         y, h = self.searching_for_insert(x, key)
@@ -126,7 +141,7 @@ class AVLTree(object):
         if y is not None:
             promoteCases = self.balance_AVLtree(y, 1, promoteCases)
 
-        return newNode, h, promoteCases
+        return newNode, h+1, promoteCases
 
 
     """inserts a new node into the dictionary with corresponding key and value, starting at the max
@@ -141,12 +156,14 @@ class AVLTree(object):
     e is the number of edges on the path between the starting node and new node before rebalancing,
     and h is the number of PROMOTE cases during the AVL rebalancing
     """
+    #O(logn)
     def finger_insert(self, key, val):
         x = self.max_node()
         newNode = self.new_node(key, val)
         if x is None:
             self.inserting_node(None, newNode)
-            return newNode, 0, 0
+            self.sizeOfTree += 1
+            return newNode, 1, 0
         edgesUp = 0
         edgesDown = 0
         if x.key < key:
@@ -165,9 +182,19 @@ class AVLTree(object):
         if y is not None:
             promoteCases = self.balance_AVLtree(y, 1, promoteCases)
 
-        return newNode, edgesUp+edgesDown, promoteCases
+        return newNode, edgesUp+edgesDown+1, promoteCases
 
+    """searching for the place to insert new node
 
+    @type key: int
+    @param key: key of item that is to be inserted to self
+    @type x: AVLNode
+    @param val: current Node from which we start searching
+    @rtype: (AVLNode,int)
+    @returns: y - parent of new node, h - edges took to find y
+    """
+
+    #O(logn)
     def searching_for_insert(self, x, key):
         y = None
         h = 0
@@ -181,6 +208,14 @@ class AVLTree(object):
         return y, h
 
 
+    """inserts a new node into the dictionary with given parent
+
+    @type par: AVLNode
+    @param par: parent of the new node
+    @type node: AVLNode
+    @param node: new node we need to insert
+    """
+    #O(1)
     def inserting_node(self, par, node):
         if par is None:
             self.root = node
@@ -197,6 +232,7 @@ class AVLTree(object):
     @type node: AVLNode
     @pre: node is a real pointer to a node in self
     """
+    #calls O(logn)
     def delete(self, node):
         if node is None:
             return
@@ -245,6 +281,7 @@ class AVLTree(object):
     @pre: all keys in self are smaller than key and all keys in tree2 are larger than key,
     or the opposite way
     """
+    #worst case O(logn)
     def join(self, tree2, key, val):
         #check who's height is bigger
         h1 = self.get_height(self.root) if self.root else -1
@@ -303,6 +340,7 @@ class AVLTree(object):
             b = t2.root
             while b.height > t1.get_height(t1.root) + 1:
                 b = b.right
+            t1.root.parent = node
             c = b.parent
             b.parent =  node
             node.parent = c
@@ -328,6 +366,8 @@ class AVLTree(object):
     dictionary smaller than node.key, and right is an AVLTree representing the keys in the 
     dictionary larger than node.key.
     """
+
+    #worst case O(h*logn) -> O((logn)^2)
     def split(self, node):
         leftTree, rightTree = AVLTree(), AVLTree()
         leftTree.root = node.left
@@ -360,12 +400,20 @@ class AVLTree(object):
                 rightTree.root.parent = None
 
         self.root = None
-        self.size = 0
+        self.sizeOfTree = 0
 
         self.detach_node(node)
 
         return leftTree, rightTree
 
+    """detaching given node from the tree
+
+    @type node: AVLNode
+    @pre: node is in self
+    @param node: the node in the dictionary we need to detach
+    """
+
+    #O(1)
     def detach_node(self, node):
         node.parent = None
         node.left = None
@@ -379,22 +427,26 @@ class AVLTree(object):
     @rtype: list
     @returns: a sorted list according to key of tuples (key, value) representing the data structure
     """
+    #calls O(n)
     def avl_to_array(self):
         avlArray = []
         self.in_order(self.root, avlArray)
         return avlArray
 
+    #O(m), m is number of nodes in subtree of currNode
     def in_order(self, currNode, avlArray):
+        if currNode is None: return
         self.in_order(currNode.left, avlArray)
-        avlArray.append(currNode)
+        avlArray.append((currNode.key, currNode.value))
         self.in_order(currNode.right, avlArray)
-        return 0
+        return
 
     """returns the node with the maximal key in the dictionary
 
     @rtype: AVLNode
     @returns: the maximal node, None if the dictionary is empty
     """
+    #O(logn)
     def max_node(self):
         currNode = self.root
         if currNode is None: return None
@@ -407,8 +459,7 @@ class AVLTree(object):
     @rtype: int
     @returns: the number of items in dictionary 
     """
-    # returns the val itself (getter)
-    # time complexity of O(1)
+    #O(1)
     def size(self):
         return self.sizeOfTree
 
@@ -418,14 +469,19 @@ class AVLTree(object):
     @rtype: AVLNode
     @returns: the root, None if the dictionary is empty
     """
-    # return the root that is in init(getter)
-    # time complexity of O(1)
+    #O(1)
     def get_root(self):
         return self.root
-    
-    #rotation func. insert a node you want to rotate and in which dir
+
+    """performs rotation
+
+    @type node: AVLNode
+    @param node: the node in the dictionary with balance factor 2 or -2
+    @type dirc: int
+    @param dirc: tells in which direction the node is to be rotated: 1 is rotation right, -1 is rotation left
+    """
+    #O(1)
     def rotation(self,nodeB,dirc):
-        # 1 is right, -1 is left
         if dirc == 1:
             nodeA = nodeB.left
             if nodeA is None : return
@@ -461,7 +517,20 @@ class AVLTree(object):
         nodeB.height = 1 + max(self.get_height(nodeB.left), self.get_height(nodeB.right))
         nodeA.height = 1 + max(self.get_height(nodeA.left), self.get_height(nodeA.right))
 
+        return
 
+
+    """performs balancing from the given node
+
+    @type node: AVLNode
+    @param node: the node in the dictionary we are starting the balancing from
+    @type dtype: int
+    @param dtype: tells if we called for balancing from insert or from delete
+    @type promoteCases: int
+    @param promoteCases: the number of rotations
+    @rtype: (int)
+    @returns: an int - number of rotations performed
+    """
     # time complexity of O(logn)
     def balance_AVLtree(self, node, dtype, promoteCases):
         # 1 for insert, -1 for delete, promoteCases is how many times we needed to rebalance (rotations cases)
@@ -507,43 +576,68 @@ class AVLTree(object):
                     self.rotation(node,-1)
             if node.parent: return self.balance_AVLtree(node.parent, dtype, promoteCases)
         return promoteCases
-    
-    #balance factor of a node
+
+    """returns the balance factor of the given node
+
+    @type node: AVLNode
+    @param node: the node in the dictionary whom balance factor is needed
+    @rtype: int
+    @returns: the balance factor of the given node
+    """
     # time complexity of O(1)
     def balance_factor(self,node):
         if node is None: return 0
         return self.get_height(node.left) - self.get_height(node.right)
     
 
-    #getter for height
+    """returns the height of the given node
+    
+    @type node: AVLNode
+    @param node: the node in the dictionary whom height is needed
+    @rtype: int
+    @returns: the height of the subtree rooted at the given node
+    """
     # time complexity of O(1)
     def get_height(self, node):
         if not node:
             return -1
         return node.height
-    
 
-    #helper to update height
+    """updates the height of a node
+
+    @type node: AVLNode
+    @param node: the node in the dictionary we need to update height to
+    """
     # time complexity of O(1)
     def update_height(self, node):
         node.height = 1 + max(self.get_height(node.left), self.get_height(node.right))
+        return
 
-
-    #helper for finding the successor
+    """returns the successor of the given node
+    
+    @type node: AVLNode
+    @param node: the node in the dictionary we need to find successor for
+    @rtype: AVLNode
+    @returns: the successor of node
+    """
     #time complexity of O(logn)
     def successor(self,node):
         if node.right is not None:
-            return self.min(node.right)
+            return self.minNode(node.right)
         y = node.parent
         while y is not None and node == y.right:
             node = y
             y = node.parent
         return y
-    
 
-    #helper to find the minimum
+    """returns the node with the minimal key in the dictionary
+
+    @rtype: AVLNode
+    @returns: the minimal node, None if the dictionary is empty
+    """
     #time complexity of O(logn)
-    def min(self,node):
+    def minNode(self,node):
+        if node is None: return None
         while node.left is not None:
             node = node.left
         return node
